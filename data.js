043308b9dce880404e4bf8b24cb384036eb412b3
Ammon Shurtz <ammonsh@outlook.com>
2020-11-03 12:11:41 -0700

    Added the data.js with a couple books. Also added their images.

---
+++ data.js
@@ -0,0 +1,17 @@
+let bookData = [{
+    id: 1,
+    title: "The Way of Kings",
+    author: "Brandon Sanderson",
+    genre: "Fantasy",
+    image: 'way-of-kings.jpg'
+},
+{
+    id: 2,
+    title: "Words of Radiance",
+    author: "Brandon Sanderson",
+    genre: "Fantasy",
+    image: 'words-of-radiance.jpg'
+}
+]
+
+export default bookData;
